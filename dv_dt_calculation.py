c03c2278b3d21901478b58d6b6734df4c9e64526
MD. Shahed <shaheddiu23@gmail.com>
2024-04-01 02:01:45 -0400

    Add new feature

--- dv_dt_calculation.py
+++ dv_dt_calculation.py
@@ -30,9 +30,35 @@ def calculate_acceleration(speed, distance):
         sum_dv_dt += dv_dt  # km/h^2
     ac = sum_dv_dt * (1 / 3.6)  # m/s
     print('dv/dt= {:.5f} m/s'.format(ac))  # m/s
+    return ac
 
 # Input
 speed = float(input('Enter EV speed (km/h): '))
 distance = float(input('Enter your distance (km): '))
 
 calculate_acceleration(speed, distance)
+
+
+# Calculate over all equation
+import math
+M = 1800  # value for Mass
+m = 1.1   # value for mass factor
+g = 9.8  # Acceleration due to gravity (m/s^2)
+f = 0.01  #
+alpha_deg = float (input('Enter Angle degree value:'))  # value for alpha in degrees
+p = 1.205  # value for p
+c = 0.6  #
+A = 3.50 #
+v0 = speed  # vehicle speed
+dv_dt0=calculate_acceleration(speed, distance)
+d=114 #total distance
+
+
+# Calculate cosine and sine of alpha
+cos_alpha=math.cos(math.radians(alpha_deg))
+sin_alpha=math.sin(math.radians(alpha_deg))
+
+# Calculate the equation
+E = (1/3600) * (M * g * (f * cos_alpha + sin_alpha) + 0.0386 * (p * c * A * v0**2) + (M + m) * dv_dt0)*d
+
+print("Toral Energy Consumption={:.5f} kWh".format(E))
